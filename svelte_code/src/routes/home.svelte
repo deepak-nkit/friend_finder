079a83dce51fac36c3d6bb39317624ccf8984882
Deepak Saini <ds954642@gmail.com>
2024-06-30 00:08:03 +0530

    Profile picture Done And other data also shown

--- svelte_code/src/routes/home.svelte
+++ svelte_code/src/routes/home.svelte
@@ -48,6 +48,7 @@
         background-color:rgb(215, 192, 192);
         padding: 10px 20px;
         width:100wh;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
     }
 
     .nav {
